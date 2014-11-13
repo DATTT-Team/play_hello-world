9d672338eedfa6498534e0efa3006ec50f2e8078
Markus Barthel <markus.barthel.mb@gmail.com>
2014-11-13 20:36:16 +0100

    mal angefangen mit Models rumzuspielen

--- play_hallo-world/app/controllers/Application.java
+++ play_hallo-world/app/controllers/Application.java
@@ -1,15 +1,21 @@
 package controllers;
 
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Hallo Play"));
-//        return ok("Hallo Play");
+        return ok(index.render("Hallo Play. Hier ist index()"));
+    }
+    
+    public static Result addUser() {
+    	User user = Form.form(User.class).bindFromRequest().get();
+    	user.save();
+    	return redirect(routes.Application.index());
     }
 
 }
