89cdc445b9e99b6279075d3cc479cec4344bffc3
Markus Barthel <markus.barthel.mb@gmail.com>
2014-11-19 20:09:04 +0100

    modeltest

--- play_hallo-world/app/models/User.java
+++ play_hallo-world/app/models/User.java
@@ -1,11 +1,17 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import play.db.ebean.Model;
 
 @Entity
 public class User extends Model{
+	
+	@Id
+	public String id;
+	
+	public String name;
 	
 
 }
