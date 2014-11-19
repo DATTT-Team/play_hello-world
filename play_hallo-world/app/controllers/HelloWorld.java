69af745bc6b1b7fc6c24f7ebfe150137585397ba
Markus Barthel <markus.barthel.mb@gmail.com>
2014-11-19 22:04:09 +0100

    Änderung2 auf feature1

--- play_hallo-world/app/controllers/HelloWorld.java
+++ play_hallo-world/app/controllers/HelloWorld.java
@@ -7,7 +7,7 @@ import views.html.*;
 public class HelloWorld extends Controller {
 	
 	public static Result helloWorld() {
-        return ok(helloworld.render("Hello World. geändert in feature2"));
+        return ok(helloworld.render("Hello World. Geändert in feature1"));
     }
 
 }
